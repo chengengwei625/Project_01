14a8a02068f125a200efd33fc2824e1237db2424
chengengwei <1213567406@qq.com>
2022-02-20 14:41:22 +0800

    创建了git忽略文件

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-19 21:30:57
+ * @LastEditTime: 2022-02-19 21:56:55
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \code\index.js
+ */
+html, body {
+    padding: 0;
+    margin: 0;
+    background - color: pink;
+}
